4ceb28420560d30decca67aaa7c9c73026d3a89e
sbund <sbund@270642c3-0616-0410-b53a-bc976706d245>
2006-05-04 13:45:19 +0000

    added index sorting

--- PacketFramework/trunk/satscons/Doxygen.py
+++ PacketFramework/trunk/satscons/Doxygen.py
@@ -7,7 +7,9 @@ def Doxygen(env, target, source):
     path, name = os.path.split(str(target))
     stamp = os.path.join(path, '.'+name+'.stamp')
     alias = env.Alias(target, env.Command(stamp, source,
-                                          [ '$DOXYGENCOM', 'touch $TARGET' ],
+                                          [ '$DOXYGENCOM',
+                                            'cd doc/html && (sed -ne \'1,/<table>/p\' <annotated.html && grep -F \'<tr>\' <annotated.html | sort -ft\'>\' -k4 && sed -ne \'/<\\/table>/,$$p\' <annotated.html) >annotated.html.new && mv annotated.html.new annotated.html',
+                                            'touch $TARGET' ],
                                           source_scanner = SCons.Defaults.ObjSourceScan))
     env.Clean(stamp, target)
     return alias
