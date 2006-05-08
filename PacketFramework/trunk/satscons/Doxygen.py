f25879565bc517bda37cea3d20e9c3d0765feec9
sbund <sbund@270642c3-0616-0410-b53a-bc976706d245>
2006-05-08 13:30:21 +0000

    Added missing (temporary) image converter makefile

--- PacketFramework/trunk/satscons/Doxygen.py
+++ PacketFramework/trunk/satscons/Doxygen.py
@@ -3,14 +3,22 @@ import SCons.Builder
 import SCons.Defaults
 import os.path
 
-def Doxygen(env, target, source):
+def replace_ext(n,ext):
+    base,ext = os.path.splitext(n)
+    return base+ext
+
+def Doxygen(env, target, source, image):
     path, name = os.path.split(str(target))
     stamp = os.path.join(path, '.'+name+'.stamp')
-    alias = env.Alias(target, env.Command(stamp, source,
-                                          [ '$DOXYGENCOM',
-                                            'cd doc/html && (sed -ne \'1,/<table>/p\' <annotated.html && grep -F \'<tr>\' <annotated.html | sort -ft\'>\' -k4 && sed -ne \'/<\\/table>/,$$p\' <annotated.html) >annotated.html.new && mv annotated.html.new annotated.html',
-                                            'touch $TARGET' ],
-                                          source_scanner = SCons.Defaults.ObjSourceScan))
+    alias = env.Alias(target,
+                      [ env.Command(os.path.splitext(img)[0]+".png", img,
+                                    [ 'TERM=dumb make -f imgconvert.mak $TARGET' ])
+                        for img in image ] +
+                      [ env.Command(stamp, source,
+                                    [ '$DOXYGENCOM',
+                                      'cd doc/html && (sed -ne \'1,/<table>/p\' <annotated.html && grep -F \'<tr>\' <annotated.html | sort -ft\'>\' -k4 && sed -ne \'/<\\/table>/,$$p\' <annotated.html) >annotated.html.new && mv annotated.html.new annotated.html',
+                                      'touch $TARGET' ],
+                                    source_scanner = SCons.Defaults.ObjSourceScan) ])
     env.Clean(stamp, target)
     return alias
 
