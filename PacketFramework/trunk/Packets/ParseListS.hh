7c973eb595b8ab727e5e02280e2bdee9d3774bdc
sbund <sbund@270642c3-0616-0410-b53a-bc976706d245>
2006-05-12 14:22:47 +0000

    Started Parse_ListS (read-only support finished)

--- PacketFramework/trunk/Packets/ParseListS.hh
+++ PacketFramework/trunk/Packets/ParseListS.hh
@@ -20,8 +20,8 @@
 // Free Software Foundation, Inc.,
 // 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#ifndef HH_ParseVec_
-#define HH_ParseVec_ 1
+#ifndef HH_ParseListS_
+#define HH_ParseListS_ 1
 
 // Custom includes
 #include <utility> // for std::pair
@@ -30,28 +30,29 @@
 #include "ParserBase.hh"
 #include "ParseArray.hh" // for Parse_Array_iterator
 
-//#include "ParseVec.mpp"
+//#include "ParseListS.mpp"
 ///////////////////////////////hh.p////////////////////////////////////////
 
 namespace satcom {
 namespace pkf {
 
-    template <class Parser, class SizeParser, class Container> class Parse_Vector_wrapper;
+    template <class Parser, class Sentinel, class Container> class Parse_ListS_wrapper;
+    namespace impl { 
+        template <class Parser, class Sentinel, class Container> class Parse_ListS_iterator; 
+    }
 
-    template <class Parser, class SizeParser, class Iterator=nil, class IPacket=nil>
-    struct Parse_Vector : public ParserBase<Iterator,IPacket>
+    template <class Parser, class Sentinel, class Iterator=nil, class IPacket=nil>
+    struct Parse_ListS : public ParserBase<Iterator,IPacket>
     {
-        typedef typename SizeParser::template rebind<Iterator>::parser size_parser;
-
         ///////////////////////////////////////////////////////////////////////////
         // Parser interface
 
         template <class I=nil, class P=nil> 
-        struct rebind { typedef Parse_Vector<Parser,SizeParser,I,P> parser; };
+        struct rebind { typedef Parse_ListS<Parser,Sentinel,I,P> parser; };
         typedef Iterator byte_iterator;
         
-        explicit Parse_Vector(SizeParser const & size);
-        Parse_Vector(size_parser const & size, Iterator const & i);
+        Parse_ListS();
+        Parse_ListS(Iterator const & i);
         
         unsigned bytes() const;
         void check(Iterator const & e) const;
@@ -61,13 +62,14 @@ namespace pkf {
         // Container interface
 
         typedef typename Parser::template rebind<Iterator>::parser value_type;
-        typedef impl::Parse_Array_iterator<value_type,Iterator> iterator;
+        typedef Sentinel sentinel;
+        typedef impl::Parse_ListS_iterator<value_type,sentinel,byte_iterator> iterator;
         typedef unsigned size_type;
         typedef int difference_type;
         typedef std::pair<iterator,iterator> range_type;
 
         template <class Container>
-        struct wrapper { typedef Parse_Vector_wrapper<value_type, size_parser, Container> t; };
+        struct wrapper { typedef Parse_ListS_wrapper<value_type, Sentinel, Container> t; };
 
         size_type size() const;
         bool empty() const;
@@ -77,20 +79,16 @@ namespace pkf {
         range_type range() const;
         range_type value() const;
 
-        value_type operator[](difference_type i) const;
-
      private:
-        size_parser size_;
-        
-        template <class P, class SP, class C> friend class Parse_Vector_wrapper;
+        template <class P, class S, class C> friend class Parse_ListS_wrapper;
     };
 
     /** \brief
         
         Holds a reference to the container !
       */
-    template <class Parser, class SizeParser, class Container>
-    class Parse_Vector_wrapper
+    template <class Parser, class Sentinel, class Container>
+    class Parse_ListS_wrapper
         : public boost::noncopyable
     {
     public:
@@ -98,10 +96,10 @@ namespace pkf {
         // Types
 
         typedef Container container;
-        typedef SizeParser size_parser;
+        typedef Sentinel sentinel;
         typedef typename Parser::byte_iterator byte_iterator;
         typedef Parser value_type;
-        typedef impl::Parse_Array_iterator<value_type,byte_iterator> iterator;
+        typedef impl::Parse_ListS_iterator<value_type,sentinel,byte_iterator> iterator;
         typedef unsigned size_type;
         typedef int difference_type;
         typedef std::pair<iterator,iterator> range_type;
@@ -110,8 +108,8 @@ namespace pkf {
         ///\name Structors and default members
         ///@{
 
-        template <class P, class SP, class I, class IP>
-        Parse_Vector_wrapper(Parse_Vector<P,SP,I,IP> const & vector, Container & container);
+        template <class P, class S, class I, class IP>
+        Parse_ListS_wrapper(Parse_ListS<P,S,I,IP> const & list, Container & container);
 
         // no default constructor
         // no copy
@@ -163,9 +161,9 @@ namespace pkf {
 }}
 
 ///////////////////////////////hh.e////////////////////////////////////////
-//#include "ParseVec.cci"
-#include "ParseVec.ct"
-#include "ParseVec.cti"
+//#include "ParseListS.cci"
+#include "ParseListS.ct"
+#include "ParseListS.cti"
 #endif
 
 
