42e499298d7d6764d5d453926094b74c25bba33e
g0dil <g0dil@270642c3-0616-0410-b53a-bc976706d245>
2008-01-18 09:40:06 +0000

    Open Daidalos TW branch

---
+++ branches/2008-01-TW/Socket/Protocols/UN/UNDatagramSocketHandle.test.cc
@@ -0,0 +1,75 @@
+// $Id$
+//
+// Copyright (C) 2007 
+// Fraunhofer Institute for Open Communication Systems (FOKUS) 
+// Competence Center NETwork research (NET), St. Augustin, GERMANY 
+//     David Wagner <dw6@berlios.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+/** \file
+    \brief UNDatagramSocketHandle.test unit tests */
+
+//#include "UNDatagramSocketHandle.test.hh"
+//#include "UNDatagramSocketHandle.test.ih"
+
+// Custom includes
+#include "UNDatagramSocketHandle.hh"
+
+#include "../../../Utils/auto_unit_test.hh"
+#include <boost/test/test_tools.hpp>
+
+#define prefix_
+///////////////////////////////cc.p////////////////////////////////////////
+
+BOOST_AUTO_UNIT_TEST(unDatagramSocketHandle)
+{
+    std::string testString ("Hallo Welt.");
+    std::string socketPath (".socket-UNDatagramSocketHandle.test");
+
+    senf::UNSocketAddress addr (socketPath);
+    senf::UNDatagramClientSocketHandle inputSocket(addr);
+    senf::UNDatagramClientSocketHandle outputSocket;
+
+
+    outputSocket.writeto( addr, testString);
+
+    BOOST_CHECK_EQUAL( inputSocket.read(), testString);
+
+      
+    
+
+    outputSocket.close();
+    inputSocket.close();
+
+//    if( unlink(socketPath.c_str()) != 0)
+//        perror( "unlink failed");
+}
+
+
+///////////////////////////////cc.e////////////////////////////////////////
+#undef prefix_
+
+
+// Local Variables:
+// mode: c++
+// fill-column: 100
+// comment-column: 40
+// c-file-style: "senf"
+// indent-tabs-mode: nil
+// ispell-local-dictionary: "american"
+// compile-command: "scons -u test"
+// End:
